2f915907509dd7a7ebb4655cb0536cae99bd5f46
Jayqueue1 <ewurajoy@yahoo.com>
2020-10-08 13:29:49 +0000

    First Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FirstComponent from './FirstComponent';
+import SecondComponent from './2ndComponent';
+function App() {
+  return (
+    <div>
+      <FirstComponent />
+      <SecondComponent />
+         </div>
+  );
+
+  }
+  export default App;
