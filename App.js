37a7dfab257f3e32385415b7852bd110218c872a
Jayqueue1 <ewurajoy@yahoo.com>
2020-10-09 01:07:52 +0000

    Commitin 3rdComponent

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import FirstComponent from './FirstComponent';
 import SecondComponent from './2ndComponent';
+import ThirdComponent from './3rdComponent';
 function App() {
   return (
     <div>
       <FirstComponent />
       <SecondComponent />
+      <ThirdComponent />
          </div>
   );
 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import FirstComponent from './FirstComponent';
 import SecondComponent from './2ndComponent';
+import ThirdComponent from './3rdComponent';
 
 ReactDOM.render(
   <React.StrictMode>
